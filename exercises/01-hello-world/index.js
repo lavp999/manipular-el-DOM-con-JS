ee2c6a98f04ec79cb2473211337caa607727b1a3
lavp999 <lavp99@hotmail.com>
2022-10-26 17:30:43 +0000

    prueba

--- exercises/01-hello-world/index.js
+++ exercises/01-hello-world/index.js
@@ -1,1 +1,17 @@
 //alert here
+
+window.onload = function(){
+
+    const categorias = ["Cocina","Economía","Deporte"];
+    const ul = document.createElement('ul');
+
+    categorias.forEach((categoria) => {
+        let li = document.createElement('li');
+        li.textContent = categoria;
+        ul.appendChild(li);
+    });
+
+    const list = document.querySelector("#list");
+    list.appendChild(ul);
+
+}
