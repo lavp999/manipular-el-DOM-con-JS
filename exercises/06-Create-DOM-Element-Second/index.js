ac4332f0485a35985481bc6fcb90ba7eafbae764
lavp999 <lavp99@hotmail.com>
2022-10-26 22:13:57 +0000

    pRUEBAS EN EL 8.2

--- exercises/06-Create-DOM-Element-Second/index.js
+++ exercises/06-Create-DOM-Element-Second/index.js
@@ -1,4 +1,4 @@
 document.body.innerHTML = "<h1>Hello World</h1>"; 
 //you code here
-
-
+let miBody =  document.body;
+miBody.innerHTML = "<img src='https://via.placeholder.com/350x150' alt=''></img>"
---
+++ exercises/08.2-Remove-DOM-Element/index.js
@@ -0,0 +1,7 @@
+// document.querySelector('#topOfStack').children;
+
+let list = document.querySelector("#parentLi");
+let childs = list.childNodes; 
+let elemento = document.getElementsByTagName("p");
+console.log(childs);
+console.log(elemento);
