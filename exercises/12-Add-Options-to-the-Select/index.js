a1332213b95f0a7000b8ee76e993bd834c65055d
lavp999 <lavp99@hotmail.com>
2022-10-28 13:45:33 +0000

    Ejercicio 12

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,12 +1,30 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 // your code here
 
-let miSelect = document.getElementById("#mySelect");
-let miOpcion = document.createElement("option");
-console.log(miSelect);
-console.log(miOpcion);
+let miSelect = document.getElementById("mySelect");
+let miOpcion;
+
+
+
+function creaAlerta(e){
+    let miSelect = document.getElementById("mySelect");
+
+    var selectedOption = this.options[miSelect.selectedIndex];
+    //console.log(selectedOption);
+
+    alert(selectedOption.value + ': ' + selectedOption.text);
+
+}
+
+
 countries.forEach(function(unPais, indice){
-                miOpcion.innerHTML = unPais;
-                miOpcion.value = indice;
-                miSelect.appendChild(miOpcion);
-            });
+    miOpcion = document.createElement("option");
+    miOpcion.setAttribute("value", "value"+indice);
+    let miOpcionTexto = document.createTextNode(unPais);
+    miOpcion.appendChild(miOpcionTexto);
+    miSelect.appendChild(miOpcion);    
+});
+        
+document.body.appendChild(miSelect);
+
+miSelect.addEventListener("change", creaAlerta, false);
