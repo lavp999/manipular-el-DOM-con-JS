0e60fac498ac6c06bdde43d632c33cee4dd55202
lavp999 <lavp99@hotmail.com>
2022-10-28 17:31:16 +0000

    13 a medias

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,11 @@
 // Your code here
+let borrar = document.getElementsByClassName("fa-trash");
+
+for(let x=0; x < borrar.length; x++){
+    borrar[x].addEventListener("click", funcionBorrar(borrar[x], x));
+}
+
+
+function funcionBorrar(elemento, indice){
+    alert("Ahí estamos " + elemento +" - "+ indice);
+}
