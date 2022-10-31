4be62bb9155c46162b426114cbaceaf80a8c9a96
lavp999 <lavp99@hotmail.com>
2022-10-31 11:31:38 +0000

    finalizado

--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,11 +1,42 @@
 // Your code here
-let borrar = document.getElementsByClassName("fa-trash");
+crearEventosDeBorrado(); //la ejecutamos la primera vez 
 
-for(let x=0; x < borrar.length; x++){
-    borrar[x].addEventListener("click", funcionBorrar(borrar[x], x));
+// ----------------------------------------------------
+//Para el borrado
+// ----------------------------------------------------
+// lo tengo creado porque al principio cuando creaba un nuevo LI llamaba a esta función
+// pero este volvería a crear todos, y los existentes ya los tengo
+function crearEventosDeBorrado(){ 
+    let miUl = document.querySelector("ul");
+    let miLi = miUl.querySelectorAll("li");
+    let miSpan = miUl.querySelectorAll("span");
+
+    for(let x=0; x < miSpan.length; x++)
+        borraElementoLi(miSpan[x], miUl, miLi[x]);
 }
 
-
-function funcionBorrar(elemento, indice){
-    alert("Ahí estamos " + elemento +" - "+ indice);
+function borraElementoLi(elemento, padre, hijo){
+    elemento.addEventListener("click", function(){
+        padre.removeChild(hijo);
+    });
 }
+
+// ----------------------------------------------------
+
+// ----------------------------------------------------
+// Para añadir
+// ----------------------------------------------------
+let nuevaAccion = document.querySelector('#addToDo');
+nuevaAccion.addEventListener('change',function(){
+    let miUl = document.querySelector('ul');
+    let nuevaLi = document.createElement('li');
+    nuevaLi.innerHTML = '<span><i class="fa fa-trash"></i></span>' + nuevaAccion.value;
+    miUl.appendChild(nuevaLi);
+    nuevaAccion.value = '';
+
+    // creamos el evento de borrado para el nuevo LI
+    let miSpan = nuevaLi.querySelector("span");
+    borraElementoLi(miSpan, miUl, nuevaLi);
+});
+// ----------------------------------------------------
+
